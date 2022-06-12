29a077ea9f84d36e23fb4306691df64189d22806
Владислав Кузьмин <sindbad1613@mail.ru>
2022-06-12 22:03:47 +0300

    merge and dev. 4S

---
+++ yatube/core/context_processors/year.py
@@ -0,0 +1,7 @@
+import datetime
+
+def year(request):
+    """Добавляет переменную с текущим годом."""
+    return {
+        'year': datetime.date.today().year
+    }
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.shortcuts import render
 
 from .models import Post, Group
 
@@ -6,12 +8,14 @@ VARIABLE_POSTS = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:VARIABLE_POSTS]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, VARIABLE_POSTS) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
-
-    return render(request, 'posts/index.html', context)
+    return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request, slug):
