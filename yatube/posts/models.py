9776faf535f104269f5d29d0dc72a93f84318ad0
Владислав Кузьмин <sindbad1613@mail.ru>
2022-07-13 11:03:13 +0500

    refac

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
     )
 
     class Meta:
-        ordering = {'-pub_date'}
+        ordering = ('-pub_date',)
 
     def __str__(self):
         return self.text[:CONSTRAINT_VARIABLE]
