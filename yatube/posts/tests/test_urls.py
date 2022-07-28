246ed2168648ded40349b61974a18dd6faf13e7d
Владислав Кузьмин <sindbad1613@mail.ru>
2022-07-28 20:13:04 +0500

    project5

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -22,7 +22,6 @@ class PostsURLTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.user = User.objects.create_user(username='author')
         cls.author = User.objects.create_user(username='author')
         cls.group = Group.objects.create(
             title='Тестовая группа',
@@ -36,11 +35,11 @@ class PostsURLTests(TestCase):
         )
 
     def setUp(self):
-        self.guest_client = Client()
-        self.post_author = Client()
-        self.post_author.force_login(self.user)
-        self.authorized_client = Client()
-        self.authorized_client.force_login(self.user)
+        self.user = User.objects.create_user(username='user')
+        self.authorized_user = Client()
+        self.authorized_user.force_login(self.user)
+        self.authorized_author = Client()
+        self.authorized_author.force_login(self.author)
 
     def test_urls_uses_correct_template(self):
         templates_url_names = {
