9f24f938e8739f5c6c9317693466cc639a3bb7a4
Владислав Кузьмин <sindbad1613@mail.ru>
2022-08-27 14:32:21 +0500

    ref

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-handler404 = 'core.views.page_not_found'
-
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
@@ -16,3 +14,7 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
