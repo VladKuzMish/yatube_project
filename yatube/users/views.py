59c059ee00924aaeb218e380eebc2cac8ab64a76
Владислав Кузьмин <sindbad1613@mail.ru>
2022-08-29 22:43:38 +0500

    ref

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -8,9 +8,3 @@ class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
-
-
-class LogIn(CreateView):
-    form_class = CreationForm
-    success_url = reverse_lazy('posts:index')
-    template_name = 'users/login.html'
